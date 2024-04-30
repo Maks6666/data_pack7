23876d9c37e405b143305ec3792f369c9fab5bf1
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-30 11:05:45 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import json
 
 class Person:
-    def __init__(self, name=None, age=None, country=None, city=None, order=None):
-        self.name = name
-        self.age = age
-        self.country = country
-        self.city = city
-        self.order = order
+    def __init__(self, question_1=None, question_2=None, question_3=None, question_4=None, question_5=None):
+        self.question_1 = question_1
+        self.question_2 = question_2
+        self.question_3 = question_3
+        self.question_4 = question_4
+        self.question_5 = question_5
 
     def add_info(self):
-        self.name = input("Input name: ")
-        self.age = int(input("Input age: "))
-        self.country = input("Input country: ")
-        self.city = input("Input city: ")
-        self.order = int(input("Input order number: "))
+        self.question_1 = input("1st question: ")
+        self.question_2 = int(input("2nd question: "))
+        self.question_3 = input("3rd question: ")
+        self.question_4 = input("4th question: ")
+        self.question_5 = int(input("5th question: "))
 
     def display_info(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Country: {self.country}")
-        print(f"City: {self.city}")
-        print(f"Order Number: {self.order}")
+        print(f"1st question answer: {self.question_1}")
+        print(f"2nd question answer: {self.question_2}")
+        print(f"3rd question answer: {self.question_3}")
+        print(f"4th question answer: {self.question_4}")
+        print(f"5th question answer: {self.question_5}")
 
     def save(self, file_name):
         dct = {
-            "name": self.name,
-            "age": self.age,
-            "country": self.country,
-            "city": self.city,
-            "order": self.order
+            "1st question": self.question_1,
+            "2nd question": self.question_2,
+            "3rd question": self.question_3,
+            "4th question": self.question_4,
+            "5th question": self.question_5
         }
 
         with open(file_name, 'w') as file:
@@ -36,41 +36,13 @@ class Person:
 
 
 
-    def load(self, file_name):
-        with open(file_name, 'r') as file:
-            dct = json.load(file)
 
-        self.name = dct["name"]
-        self.age = dct["age"]
-        self.country = dct["country"]
-        self.city = dct["city"]
-        self.order = dct["order"]
+number = int(input("How many objects do ypu want to create? "))
+for i in range(number):
+    person = Person()
+    person.add_info()
+    person.display_info()
+    person.save(f"data_{i+1}.json")
 
-    def change_info(self, file_name):
-        self.name = input("Input new name: ")
-        self.age = int(input("Input new age: "))
-        self.country = input("Input new country: ")
-        self.city = input("Input new city: ")
-        self.order = int(input("Input new order number: "))
-
-        dct = {
-            "name": self.name,
-            "age": self.age,
-            "country": self.country,
-            "city": self.city,
-            "order": self.order
-        }
-
-        with open(file_name, 'w') as file:
-            json.dump(dct, file)
-
-
-
-person = Person()
-person.add_info()
-person.display_info()
-person.save("data.json")
-person.load("data.json")
-person.change_info("data.json")
 
 
